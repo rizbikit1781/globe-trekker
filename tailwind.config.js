ecced602c1d8bc7c98c986a01e84227c1b5d1728
rizbikit1781 <112736862+rizbikit1781@users.noreply.github.com>
2024-07-02 23:55:14 -0600

    Set up the project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        orange: "#e8604c",
+        textColor: "#313041",
+        gray: "#A9A8B6"
+      }
+    },
+  },
+  plugins: [],
+}
+
